ea3c0543b3ecab085013e8b7ee7eae1b0191d87a
l7960261 <l7960261@gmail.com>
2018-08-09 18:44:43 +0800

    add address

--- src/app/pages/survey-page/survey-page.component.ts
+++ src/app/pages/survey-page/survey-page.component.ts
@@ -14,6 +14,8 @@ export class SurveyPageComponent implements OnInit, OnDestroy {
   uid: string;
   organizer: string;
   attends = [{ text: '是', value: 1 }, { text: '否', value: 0 }];
+  invitations = [{ text: '需要，請寄紙本喜帖給我', value: 1 }, { text: '不用唷，婚禮相關資訊我知道了', value: 0 }];
+  address: string;
   surveyForm: FormGroup;
   surveyRead$: Subscription;
   surveyWrite$: Subscription;
@@ -24,13 +26,18 @@ export class SurveyPageComponent implements OnInit, OnDestroy {
     this.caption = this.params.data.caption;
     this.uid = this.params.data.uid;
     this.organizer = this.params.data.organizer;
+
     this.surveyForm = this.fb.group({
-      attend: [0]
+      attend: [1],
+      invitation: [0]
     });
+    this.address = '';
 
     this.surveyRead$ = this.surveyService.read(this.uid, this.organizer).subscribe(data => {
       if (data) {
         this.surveyForm.get('attend').setValue(data.attend);
+        this.surveyForm.get('invitation').setValue(data.invitation || 0);
+        this.address = data.address || '';
       }
     });
   }
@@ -43,7 +50,7 @@ export class SurveyPageComponent implements OnInit, OnDestroy {
   }
 
   onSubmit() {
-    const data = Object.assign(this.surveyForm.value, { uid: this.uid });
+    const data = Object.assign(this.surveyForm.value, { uid: this.uid, address: this.address });
     this.surveyWrite$ = this.surveyService.write(data, this.organizer).subscribe(() => {
       alert('提交成功');
     });
