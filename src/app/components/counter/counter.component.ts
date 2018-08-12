8ff5f01a7143c03687a60287efd5305af77fe4c1
l7960261 <l7960261@gmail.com>
2018-08-12 12:31:21 +0800

    modify height for login-page

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SurveyService } from '../../core/survey/survey.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { AuthenticationService } from '../../core/authentication/authentication.service';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent implements OnInit {
-  constructor(private surveyService: SurveyService) {}
+export class CounterComponent implements OnInit, OnDestroy {
+  constructor(private surveyService: SurveyService, private authenticationService: AuthenticationService) {}
 
+  isSignIn: boolean;
   membersTotal$: Observable<number>;
+  user$: Subscription;
 
   ngOnInit() {
     this.membersTotal$ = this.surveyService.membersTotal();
+    this.user$ = this.authenticationService.user().subscribe(user => {
+      this.isSignIn = !!user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.user$.unsubscribe();
   }
 }
