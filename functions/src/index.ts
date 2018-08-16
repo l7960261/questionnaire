a4c454ebba9a51301b6a2b5fc248c7e24260af59
l7960261 <l7960261@gmail.com>
2018-08-16 09:45:10 +0800

    modify firebase.json

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import * as functions from 'firebase-functions';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const surveyEmail = functions.database.ref('/surveys/{uid}/{organizer}').onWrite((change, context) => {
+export const surveyDetect = functions.database.ref('/surveys/{uid}/{organizer}').onWrite((change, context) => {
   console.log('Survey by: ', context.params.uid, ', organizer: ', context.params.organizer);
   console.log('Value: ', change.after.val());
 
