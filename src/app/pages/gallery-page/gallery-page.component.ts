c9ec2bba0309c6d162a568431b6db28fd421d8ad
l7960261 <l7960261@gmail.com>
2018-10-19 15:23:37 +0800

    modify gallery-page

--- src/app/pages/gallery-page/gallery-page.component.ts
+++ src/app/pages/gallery-page/gallery-page.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../../core/menu/menu.service';
-import * as _ from 'lodash';
 
 @Component({
   selector: 'ons-page',
@@ -10,21 +9,9 @@ import * as _ from 'lodash';
 export class GalleryPageComponent implements OnInit {
   constructor(private menuService: MenuService) {}
 
-  photos = _.range(8).map(v => `/assets/images/weddingphotos/${v}.jpg`);
-  @ViewChild('modal')
-  modal: any;
-
-  @ViewChild('carousel')
-  carousel: any;
-
   ngOnInit() {}
 
   openMenu() {
     this.menuService.open();
-  }
-
-  openModal(index: number) {
-    this.carousel.nativeElement.setActiveIndex(index);
-    this.modal.nativeElement.show();
   }
 }
